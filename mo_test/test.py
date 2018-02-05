7eaa3c9fb1c96495ebc0a1be775c05d08adbdfb6
MiracleYoung <miracleyoung0723@gmail.com>
2018-02-05 08:18:29 +0800

    test update.

--- mo_test/test.py
+++ mo_test/test.py
@@ -5,23 +5,28 @@ import base64
 import hashlib
 import struct
 
-# ====== config ======
 HOST = 'localhost'
-PORT = 3368
+PORT = 1235
 MAGIC_STRING = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
 HANDSHAKE_STRING = "HTTP/1.1 101 Switching Protocols\r\n" \
                    "Upgrade:websocket\r\n" \
                    "Connection: Upgrade\r\n" \
-                   "Sec-WebSocket-Accept: {1}\r\n" \
-                   "WebSocket-Location: ws://{2}/chat\r\n" \
+                   "Sec-WebSocket-Accept: {0}\r\n" \
+                   "WebSocket-Location: ws://{1}:{2}/chat\r\n" \
                    "WebSocket-Protocol:chat\r\n\r\n"
 
 
 class Th(threading.Thread):
-    def __init__(self, connection, ):
-        threading.Thread.__init__(self)
-        self.con = connection
+    def __init__(self, conn):
+        super(Th, self).__init__()
+        self.con = conn
 
+    def run(self):
+        while True:
+            try:
+                pass
+            except:
+                self.con.close()
 
     def recv_data(self, num):
         try:
@@ -42,19 +47,16 @@ class Th(threading.Thread):
                 masks = all_data[2:6]
                 data = all_data[6:]
             raw_str = ""
-            i = 0
-            for d in data:
+            for i, d in enumerate(data):
                 raw_str += chr(ord(d) ^ ord(masks[i % 4]))
-                i += 1
-            return raw_str
+        return raw_str
 
-    # send data
     def send_data(self, data):
         if data:
-            data = str(data)
+            data = data
         else:
             return False
-        token = "\x81"
+        token = b"\x81"
         length = len(data)
         if length < 126:
             token += struct.pack("B", length)
@@ -63,74 +65,66 @@ class Th(threading.Thread):
         else:
             token += struct.pack("!BQ", 127, length)
         # struct为Python中处理二进制数的模块，二进制流为C，或网络流的形式。
-
-        data = '%s%s' % (token, data)
+        data = token + data
         self.con.send(data)
         return True
 
-# handshake
-def handshake(con):
+
+def handshake(conn):
     headers = {}
-    shake = con.recv(1024)
+    shake = conn.recv(1024).decode()
 
     if not len(shake):
         return False
 
-    header, data = shake.split('\r\n\r\n', 1)
-    for line in header.split('\r\n')[1:]:
+    data, _ = shake.split('\r\n\r\n', 1)
+    for line in data.split('\r\n')[1:]:
         key, val = line.split(': ', 1)
         headers[key] = val
 
     if 'Sec-WebSocket-Key' not in headers:
         print('This socket is not websocket, client close.')
-        con.close()
+        conn.close()
         return False
 
     sec_key = headers['Sec-WebSocket-Key']
-    res_key = base64.b64encode(hashlib.sha1(sec_key + MAGIC_STRING).digest())
+    res_key = base64.b64encode(hashlib.sha1((sec_key + MAGIC_STRING).encode()).digest())
 
-    str_handshake = HANDSHAKE_STRING.replace('{1}', res_key).replace('{2}', HOST + ':' + str(PORT))
+    str_handshake = HANDSHAKE_STRING.format(res_key.decode(), HOST, str(PORT))
     print(str_handshake)
-    con.send(str_handshake)
+    conn.send(str_handshake.encode())
     return True
 
 
-def new_service():
+def new_service(ip, port):
     """start a service socket and listen
     when coms a connection, start a new thread to handle it"""
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        sock.bind(('localhost', 3368))
+        sock.bind((ip, port))
         sock.listen(1000)
-        # 链接队列大小
-        print("bind 3368,ready to use")
+        print("bind {0}:{1},ready to use".format(ip, port))
     except:
         print("Server is already running,quit")
         sys.exit()
 
     while True:
-        connection, address = sock.accept()
+        conn, address = sock.accept()
         # 返回元组（socket,add），accept调用时会进入waite状态
         print("Got connection from ", address)
-        if handshake(connection):
+        if handshake(conn):
             print("handshake success")
             try:
-                t = Th(connection)
+                t = Th(conn)
                 t.start()
                 print('new thread for client ...')
-            except:
+            except Exception as e:
                 print('start new thread error')
-                connection.close()
+            except KeyboardInterrupt:
+                conn.close()
 
-
-def main(port):
-    so = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    so.bind(('0.0.0.0', port))
-    so.listen()
-    s, info = so.accept()
-    print(s.recv(1024))
 
 if __name__ == '__main__':
-    _port = int(sys.argv[1])
-    main(_port)
+    # _port = int(sys.argv[1])
+    new_service(HOST, PORT)
